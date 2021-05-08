cd3164a9e80b02f154f1fcdee68e79f78151e9b0
Sekam Dex <sekamdex@gmail.com>
2021-05-08 09:04:25 -0500

    subscriptions-transport-ws and conf. subscription

--- src/Features/DataProvider/DataProvider.tsx
+++ src/Features/DataProvider/DataProvider.tsx
@@ -1,10 +1,21 @@
 import React, { ReactNode } from 'react';
-import { Provider, createClient } from 'urql';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
+import { Provider, defaultExchanges, subscriptionExchange, createClient } from 'urql';
 
 type Props = { children: ReactNode }
 
+const subscriptionClient = new SubscriptionClient('ws://react.eogresources.com/graphql', {
+  reconnect: true,
+});
+
 const client = createClient({
   url: 'https://react.eogresources.com/graphql',
+  exchanges: [
+    ...defaultExchanges,
+    subscriptionExchange({
+      forwardSubscription: operation => subscriptionClient.request(operation),
+    }),
+  ],
 });
 
 export default ({ children }: Props) => {
