d54a6a445fb064a8d01ef14d2c37185f815fba0d
Sekam Dex <sekamdex@gmail.com>
2021-05-08 00:38:04 -0500

    setting MetricSelection state to hold metrics list

--- src/Features/MetricSelection/MetricSelection.tsx
+++ src/Features/MetricSelection/MetricSelection.tsx
@@ -1,6 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useQuery } from 'urql';
+import { useDispatch } from 'react-redux';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import { actions } from '../DataProvider/reducerMetrics';
+
+const query = `
+query {
+  getMetrics
+}
+`;
 
 export default () => {
+  const dispatch = useDispatch();
+  const [metrics, setMetrics] = useState([]);
+  const [result] = useQuery({ query });
+  const { fetching, data, error } = result;
+
+  useEffect(() => {
+    if (error) {      
+      dispatch(actions.metricsApiErrorReceived({ error: error.message }));
+      return;
+    }
+    if (!data) return;
+    setMetrics(data.getMetrics);
+  }, [data, error, dispatch]); 
+
+  if (fetching) return <LinearProgress />;
+
   return (
     <div>
       Metric Selection
