196c5edb919255b56d79e1ac729813017509b616
Sekam Dex <sekamdex@gmail.com>
2021-05-08 12:45:15 -0500

    formatted data through saga - nivo format

--- src/Features/DataProvider/sagaMeasurementsMultiple.ts
+++ src/Features/DataProvider/sagaMeasurementsMultiple.ts
@@ -3,9 +3,28 @@ import { toast } from 'react-toastify';
 import { PayloadAction } from 'redux-starter-kit';
 import { actions as MeasurementsActions, MultipleMeasurements } from './reducerMeasurements';
 
-function* multipleMeasurementsReceived(action: PayloadAction<MultipleMeasurements>) {
-  console.log(action.payload);  
-  // yield call(toast.error, `Error Received: ${action.payload.error}`);
+export type DataSetUnit = {
+  x: number;
+  y: number;
+};
+
+export type DataSet = {
+  id: string;
+  data: DataSetUnit[];
+};
+
+export type Data = DataSet[];
+
+function* multipleMeasurementsReceived(action: PayloadAction<MultipleMeasurements[]>) {
+  const data = action.payload.map((meas) => (
+    {
+      id: meas.metric,
+      data: meas.measurements.map((item) => ({
+        x: item.at,
+        y: item.value
+      }))
+    }
+  ));
 }
 
 export default function* watchSelection() {
