8b3a84979a0caa251b7389034e644a27d64d3546
Sekam Dex <sekamdex@gmail.com>
2021-05-08 08:59:59 -0500

    basic card implementation

--- src/Features/Cards/Cards.tsx
+++ src/Features/Cards/Cards.tsx
@@ -1,9 +1,50 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Card from '@material-ui/core/Card';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import CardContent from '@material-ui/core/CardContent';
+import { IState } from '../../store';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  title: {
+    fontSize: 14,
+  }
+});
+
+const getMetrics = (state: IState) => state.metrics;
 
 export default () => {
+  const metrics = useSelector(getMetrics);
+
   return (
-    <div>
-      Cards
-    </div>
+    <Grid container spacing={3}>
+      {metrics.map((metric, index) => (
+        <Grid item key={index} xs={12} sm={6} lg={3}>
+          <CustomCard metric={metric} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
+const CustomCard = ({ metric = '' }) => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography className={classes.title} gutterBottom>
+          {metric}
+        </Typography>
+        <Typography variant="h5">
+          Value here
+        </Typography>
+      </CardContent>
+    </Card>
   );
 };
