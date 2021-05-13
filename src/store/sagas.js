c54b7d27fbbe92898d00d1b8ebe2cdc9407ff147
Sekam Dex <sekamdex@gmail.com>
2021-05-12 20:46:20 -0500

    removed unused sagas

--- src/store/sagas.js
+++ src/store/sagas.js
@@ -2,15 +2,11 @@ import { spawn, all } from 'redux-saga/effects';
 import weatherSaga from '../Features/Weather/saga';
 import metricsErrorSaga from '../Features/DataProvider/sagaMetricsError';
 import measurementsErrorSaga from '../Features/DataProvider/sagaMeasurementsError';
-import measurementsMultipleSaga from '../Features/DataProvider/sagaMeasurementsMultiple';
-import graphPointSaga from '../Features/DataProvider/sagaGraphPoint';
 
 export default function* root() {
   yield all([
     spawn(weatherSaga),
     spawn(metricsErrorSaga),
     spawn(measurementsErrorSaga),
-    spawn(measurementsMultipleSaga),
-    spawn(graphPointSaga),
   ]);
 }
